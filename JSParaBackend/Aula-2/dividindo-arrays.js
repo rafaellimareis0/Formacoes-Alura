18de8beaca949659d236581a50eed854f5b128c3
rafaellimareis0 <rafaellimareis0@gmail.com>
2021-11-19 13:49:08 -0300

    Aprendendo split()

--- JSParaBackend/Aula-2/dividindo-arrays.js
+++ JSParaBackend/Aula-2/dividindo-arrays.js
@@ -1,5 +1,7 @@
 const nomes = ['João', 'Juliana', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Guilherme','Aline','Fabiana','Andre','Carlos','Paulo','Bia','Vivian','Isabela','Vinicius','Renan','Renata','Daisy','Camilo'];
 
+console.log("Tamanho da array: ", nomes.length)
+
 const sala1 = nomes.slice(0, nomes.length/2)
 const sala2 = nomes.slice(nomes.length/2)
 
