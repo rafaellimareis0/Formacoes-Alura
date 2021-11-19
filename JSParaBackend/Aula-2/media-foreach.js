385025efb42056fe412a0f2fcb7d671451442e89
rafaellimareis0 <rafaellimareis0@gmail.com>
2021-11-19 17:52:48 -0300

    Aprendendo comandos for/forEach

---
+++ JSParaBackend/Aula-2/media-foreach.js
@@ -0,0 +1,17 @@
+const notas = [10, 6.5, 8, 7.5]
+
+let somaDasNotas = 0;
+
+//com arrow function
+// notas.forEach(nota => {
+//     somaDasNotas += nota
+// })
+
+//sem arrow function
+notas.forEach(function(nota){
+    somaDasNotas += nota
+})
+
+let media = somaDasNotas / notas.length;
+
+console.log(`A media das notas é ${media}`)
