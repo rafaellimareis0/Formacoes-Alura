89008883768a20156cb787905834a3b0ffdbd31c
rafaellimareis0 <rafaellimareis0@gmail.com>
2021-11-22 18:40:30 -0300

    Finalizando a aula 2 do curso de JS para Back-end da Alura.

---
+++ JSParaBackend/Aula-2/media-geral.js
@@ -0,0 +1,28 @@
+const salaJS = [7, 8, 8, 7, 10, 6.5, 4 , 10, 7]
+const salaJava = [6, 5, 8, 9, 5, 6]
+const salaPython = [7, 3.5, 8, 9.5]
+
+function mediaSala(notasDaSala){
+    const somaDasNotas = notasDaSala.reduce((acum, atual) => atual + acum, 0)
+    return somaDasNotas / notasDaSala.length
+}
+console.log(`Media da sala de JavaScript: ${mediaSala(salaJS)}.`)
+console.log(`Media da sala de Java: ${mediaSala(salaJava)}.`)
+console.log(`Media da sala de Python: ${mediaSala(salaPython)}.`)
+
+
+//ultimo teste
+
+// const notas = [10, 6.5, 8, 7]
+
+// function media(notasGeral){
+//     const somaGeral = notasGeral.reduce((acum, atual) => atual + acum, 0)
+//     return somaGeral / notasGeral.length
+// }
+// console.log(`Media geral: ${media(notas)}`)
+
+
+//outro jeito de fazer o de cima
+// const notas = [10, 6.5, 8, 7]
+// const media = notas.reduce((acum, atual) => atual + acum, 0) / notas.length
+// console.log(media)
