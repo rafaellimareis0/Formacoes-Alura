c795764358f491ede79709df89c820ded111b388
rafaellimareis0 <rafaellimareis0@gmail.com>
2021-12-09 18:16:46 -0300

    Teste Fizzbuzz.js

---
+++ JSParaBackend/Aula-1/teste-fizzbuzz.js
@@ -0,0 +1,22 @@
+let i;
+for (i=1; i<=15; i++)
+{
+    // number divisible by 3 and 5 will
+    // always be divisible by 15, print
+    // 'FizzBuzz' in place of the number
+    if (i%15 == 0)   
+        console.log("FizzBuzz" + " ");
+     
+    // number divisible by 3? print 'Fizz'
+    // in place of the number
+    else if ((i%3) == 0)
+        console.log("Fizz" + " ");               
+     
+    // number divisible by 5, print 'Buzz'
+    // in place of the number
+    else if ((i%5) == 0)                   
+        console.log("Buzz" + " ");               
+ 
+    else // print the number       
+        console.log(i + " ");               
+}
