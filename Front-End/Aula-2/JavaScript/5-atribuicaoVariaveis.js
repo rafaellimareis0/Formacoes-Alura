baab19aeac09c4814e14f202e49472f7c1ad4156
rafaellimareis0 <rafaellimareis0@gmail.com>
2021-12-13 11:27:34 -0300

    Novas operaçoes

--- Front-End/Aula-2/JavaScript/5-atribuicaoVariaveis.js
+++ Front-End/Aula-2/JavaScript/5-atribuicaoVariaveis.js
@@ -1,14 +1,16 @@
 console.log("Trabalhando com atribuição de variáveis");
-const idade = 29;
+
+const idade = 30;
 const primeiroNome = "Rafael";
 const sobrenome = "Lima";
 
-//console.log(nome + " " + sobrenome)
-console.log(primeiroNome, sobrenome)
-console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
 
-let contador = 0
-contador = contador +1
-const nomeCompleto = primeiroNome + sobrenome;
-console.log(nomeCompleto);
-nomeCompleto =2;
+console.log(`Meu nome é ${primeiroNome} ${sobrenome} e minha idade é de ${idade} anos.`);
+
+let valor1 = 1;
+let valor2 = 2;
+let valor3 = 3;
+
+let valorSomado = valor1 + valor2 + valor3;
+
+console.log(`O valor da soma em questão é de ${valorSomado}`)
