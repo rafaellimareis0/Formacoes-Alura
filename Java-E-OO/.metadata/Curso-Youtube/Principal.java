088d01049d6b21adca4005083ead42a618b59f91
rafaellimareis0 <rafaellimareis0@gmail.com>
2022-01-07 11:33:59 -0300

    Cursos youtub

---
+++ Java-E-OO/.metadata/Curso-Youtube/Principal.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String args[]) {
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Entre com seu nome");
+        nome = ler.nextLine();
+
+        System.out.println("Olá "+ nome);
+    }
+}
