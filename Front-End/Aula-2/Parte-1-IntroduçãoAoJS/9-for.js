a415a27056a09fb09e0c7856cf438db6fddb1aaf
rafaellimareis0 <rafaellimareis0@gmail.com>
2022-01-10 17:17:13 -0300

    Testando funcionalidades no Windows 11

--- Front-End/Aula-2/Parte-1-IntroduçãoAoJS/9-for.js
+++ Front-End/Aula-2/Parte-1-IntroduçãoAoJS/9-for.js
@@ -42,6 +42,8 @@ for (i = 0; i < 8; i++) {
 //     i += 1; //aqui vai contando até o if for verdadeiro
 // }
 
+//teste do git/github
+
 console.log("Destino existe?", destinoExiste)
 
 if (podeComprar && destinoExiste) {
