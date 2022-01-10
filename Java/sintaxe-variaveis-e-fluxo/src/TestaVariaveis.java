1e1191a82364310a5d0e1243e8ff73a9f7828850
rafaellimareis0 <rafaellimareis0@gmail.com>
2022-01-10 16:01:29 -0300

    Aprendendo sobre variaveis

--- Java/sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
+++ Java/sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -1,7 +1,17 @@
 
 public class TestaVariaveis {
-	
+
 	public static void main(String[] Args) {
-		System.out.println("Olá novo teste!");
+		System.out.println("Aprendendo sobre variáveis primitivas");
+
+		String nome, sobrenome; // String para definir o tipo de variavel e nome/sobrenome para o nome da
+								// variavel
+		int idade, anoDeNascimento; // int para definir o tipo, idade/anoDeNascimento como nome da variavel
+		idade = 10 + 20; // atribuindo valor a variavel
+		anoDeNascimento = 1991; // atribuindo valor a variavel
+		nome = "Rafael"; // atribuindo valor a variavel
+		sobrenome = "Lima"; // atribuindo valor a variavel
+		System.out.println("Meu nome é " + nome + " " + sobrenome + ", a minha idade é " + idade
+				+ " anos e eu nasci no ano de " + anoDeNascimento);
 	}
 }
