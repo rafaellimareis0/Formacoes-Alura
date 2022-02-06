3bdb385f9144930bd6498d0e37c8b275f1d1f118
rlreisdev <rafaellimareis0@gmail.com>
2022-02-06 17:35:45 -0300

    Limpando codigo em JS

---
+++ EstudosPessoais/vitor.js
@@ -0,0 +1,27 @@
+
+
+
+console.log(`Exercicio do Vitor`);
+
+//Aqui é com WHILE
+var limite = 40;
+var i = 30;
+while (i <= limite) {
+    if (i == 33 || i == 37){
+        i++;
+    } else {
+    console.log(`Valor do contador ${i}`);
+    i++;
+    }
+}
+console.log("FIM");
+
+
+//Aqui é com FOR
+for (let i = 30; i <= 40; i++) {
+    if (i == 33 || i == 37){
+    } else {
+        console.log(`Valor do contador ${i}`);
+    }
+}
+console.log("FIM");
